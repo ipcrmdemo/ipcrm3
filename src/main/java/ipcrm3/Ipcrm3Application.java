658f5f142ee04f544de3fea97370716a93e09ab2
Matt Cadorette <mcadorette@gmail.com>
2018-11-01 16:20:35 -0400

    Initial commit from Atomist

---
+++ src/main/java/ipcrm3/Ipcrm3Application.java
@@ -0,0 +1,12 @@
+package ipcrm3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipcrm3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipcrm3Application.class, args);
+	}
+}
---
+++ src/test/java/ipcrm3/Ipcrm3ApplicationTests.java
@@ -0,0 +1,16 @@
+package ipcrm3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ipcrm3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
